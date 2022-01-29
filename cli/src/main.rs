e58c0f3fd5efca4e39caaeca04a615de4d0b1785
Emil H. Clausen <sequoiia@hummel.yt>
2022-01-29 15:05:59 +0100

    Partial consumer group member metadata parsing

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let mut cm = kaf9s_core::config::ConfigManager::load();
+    let cm = kaf9s_core::config::ConfigManager::load();
     let current_context = cm.get_current_context();
 
     let kafka_conf = cm.context_to_kafka_config(&current_context.name).unwrap();
 
     println!("Connecting to cluster {} with user {}", current_context.cluster, current_context.user);
 
-    //kaf9s_core::topic::get_consumer_groups();
-    kaf9s_core::topic::get_topics(kafka_conf);
+    kaf9s_core::topic::get_consumer_groups(kafka_conf.clone());
+    //kaf9s_core::topic::get_topics(kafka_conf);
 }
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -10,35 +10,46 @@ pub mod error;
 pub mod config;
 
 pub mod topic {
+    use std::io::BufRead;
     use rdkafka::consumer::{BaseConsumer, Consumer};
     use rdkafka::config::RDKafkaLogLevel;
     use std::time::Duration;
-    use rdkafka::ClientConfig;
+    use byteorder::ReadBytesExt;
+    use rdkafka::{ClientConfig, TopicPartitionList};
 
-    pub fn get_consumer_groups() {
-        let consumer : BaseConsumer = rdkafka::config::ClientConfig::new()
-            .set("bootstrap.servers", "")
-            .set("sasl.mechanisms", "PLAIN")
-            .set("sasl.username", "")
-            .set("sasl.password", "")
-            .set("security.protocol", "SASL_SSL")
-            .set_log_level(RDKafkaLogLevel::Debug)
+    pub fn get_consumer_groups(conf : ClientConfig) {
+        let consumer : BaseConsumer = conf
             .create()
             .expect("Unable to create consumer");
 
-        let metadata = consumer
-            .fetch_metadata(None, std::time::Duration::from_secs(10)).expect("Failed to fetch metadata");
         let groups = consumer.fetch_group_list(None, std::time::Duration::from_secs(10)).expect("Unable to get consumer groups");
 
         println!("Groups:");
         for rd_group in groups.groups() {
             println!("  Name: {}", rd_group.name());
+            println!("  State: {}", rd_group.state());
+            println!("  Protocol: {}", rd_group.protocol());
+            println!("  Protocol type: {}", rd_group.protocol_type());
             println!("  Members:");
 
             for member in rd_group.members() {
                 println!("    Name: {}", member.id());
                 println!("    Client host: {}", member.client_host());
                 println!("    Client id: {}", member.client_id());
+                println!("    Topics:");
+                if member.metadata().is_some() && rd_group.protocol_type().eq("consumer") {
+                    // Based upon @messense work at https://github.com/fede1024/rust-rdkafka/pull/184/files
+                    let payload = member.metadata().unwrap();
+                    let mut cursor = std::io::Cursor::new(payload);
+                    let _version = cursor.read_i16::<byteorder::BigEndian>().unwrap();
+                    let assign_len = cursor.read_i32::<byteorder::BigEndian>().unwrap();
+                    for _ in 0..assign_len {
+                        let topic = read_str(&mut cursor)
+                            .unwrap()
+                            .to_string();
+                        println!("      {}", topic);
+                    }
+                }
             }
         }
     }
@@ -51,7 +62,6 @@ pub mod topic {
         let metadata = consumer
             .fetch_metadata(None, std::time::Duration::from_secs(10)).expect("Failed to fetch metadata");
 
-        let mut message_count = 0;
 
         println!("Cluster information:");
         println!("  Broker count: {}", metadata.brokers().len());
@@ -59,18 +69,9 @@ pub mod topic {
         println!("  Metadata broker name: {}", metadata.orig_broker_name());
         println!("  Metadata broker id: {}\n", metadata.orig_broker_id());
 
-        // println!("Brokers:");
-        // for broker in metadata.brokers() {
-        //     println!(
-        //         "  Id: {}  Host: {}:{}  ",
-        //         broker.id(),
-        //         broker.host(),
-        //         broker.port()
-        //     );
-        // }
-
         println!("\nTopics:");
         for topic in metadata.topics() {
+            let mut message_count = 0;
             println!("  Topic: {}  Err: {:?}", topic.name(), topic.error());
             for partition in topic.partitions() {
                 println!(
@@ -96,5 +97,16 @@ pub mod topic {
         }
     }
 
+    fn read_str<'a>(rdr: &'a mut std::io::Cursor<&[u8]>) -> Result<&'a str, Box<dyn std::error::Error>> {
+        let len = (rdr.read_i16::<byteorder::BigEndian>())? as usize;
+        let pos = rdr.position() as usize;
+        let slice = std::str::from_utf8(&rdr.get_ref()[pos..(pos + len)])?;
+        rdr.consume(len);
+        Ok(slice)
+    }
 
+    pub struct MemberAssignment {
+        pub topic : String,
+        pub partitions : Vec<i32>
+    }
 }
